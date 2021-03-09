064238efe1115bec5976bc639e27f57e02b4de7b
Sindre Sogge Heggen <sindre.heggen@gmail.com>
2021-03-09 07:46:10 +0100

    video om einingar

--- src/pages/tital.js
+++ src/pages/tital.js
@@ -9,6 +9,7 @@ const vids = () => (
 	<Layout>
 	  <Vids>
  <li><a href="https://drive.google.com/file/d/1GeP-37n72vwuGtOMrxgUiMtQNG_9HrC6/view?usp=sharing"> Tidelar og hundredelar</a></li>
+  <li><a href="https://drive.google.com/file/d/1S0m3dRRWSHp8cJ9qfGBJWrbPWGMn4PQx/view?usp=sharing"> Omgjering av einingar</a></li>
 	  </Vids>
 	  <Opg>
 	  </Opg>
