752d4fadd1ae3c3cd0ab9b9ff699d8e8fa378a96
Sindre Sogge Heggen <sindre.heggen@gmail.com>
2021-06-08 15:03:21 +0200

    v3

--- .cache/_this_is_virtual_fs_path_/$virtual/async-requires.js
+++ .cache/_this_is_virtual_fs_path_/$virtual/async-requires.js
@@ -2,6 +2,7 @@
 const preferDefault = m => (m && m.default) || m
 
 exports.components = {
+  "component---cache-dev-404-page-js": () => import("./../../dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
   "component---src-pages-404-js": () => import("./../../../src/pages/404.js" /* webpackChunkName: "component---src-pages-404-js" */),
   "component---src-pages-bok-js": () => import("./../../../src/pages/bok.js" /* webpackChunkName: "component---src-pages-bok-js" */),
   "component---src-pages-brok-js": () => import("./../../../src/pages/brok.js" /* webpackChunkName: "component---src-pages-brok-js" */),
