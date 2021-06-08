9eb2caa80eccaeee265a3512a0f126677f6aefbd
Sindre Sogge Heggen <sindre.heggen@gmail.com>
2021-06-08 11:38:50 +0200

    gatsby3

--- .cache/_this_is_virtual_fs_path_/$virtual/sync-requires.js
+++ .cache/_this_is_virtual_fs_path_/$virtual/sync-requires.js
@@ -10,10 +10,8 @@ exports.components = {
   "component---src-pages-c-1-js": preferDefault(require("/home/sindre/web/matte/src/pages/c1.js")),
   "component---src-pages-c-2-js": preferDefault(require("/home/sindre/web/matte/src/pages/c2.js")),
   "component---src-pages-index-js": preferDefault(require("/home/sindre/web/matte/src/pages/index.js")),
-  "component---src-pages-pdf-gen-js": preferDefault(require("/home/sindre/web/matte/src/pages/pdf_gen.js")),
   "component---src-pages-pdf-out-js": preferDefault(require("/home/sindre/web/matte/src/pages/pdf_out.js")),
   "component---src-pages-rekning-js": preferDefault(require("/home/sindre/web/matte/src/pages/rekning.js")),
-  "component---src-pages-t-1-js": preferDefault(require("/home/sindre/web/matte/src/pages/t1.js")),
   "component---src-pages-t-2-js": preferDefault(require("/home/sindre/web/matte/src/pages/t2.js")),
   "component---src-pages-tital-js": preferDefault(require("/home/sindre/web/matte/src/pages/tital.js")),
   "component---src-pages-using-typescript-tsx": preferDefault(require("/home/sindre/web/matte/src/pages/using-typescript.tsx"))
