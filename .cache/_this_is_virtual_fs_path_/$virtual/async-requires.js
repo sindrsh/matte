374a26c613836458319a04e1170f96d7b710b7d0
Sindre Sogge Heggen <sindre.heggen@gmail.com>
2021-06-08 14:02:14 +0200

    added exercise generator

--- .cache/_this_is_virtual_fs_path_/$virtual/async-requires.js
+++ .cache/_this_is_virtual_fs_path_/$virtual/async-requires.js
@@ -6,13 +6,10 @@ exports.components = {
   "component---src-pages-404-js": () => import("./../../../src/pages/404.js" /* webpackChunkName: "component---src-pages-404-js" */),
   "component---src-pages-bok-js": () => import("./../../../src/pages/bok.js" /* webpackChunkName: "component---src-pages-bok-js" */),
   "component---src-pages-brok-js": () => import("./../../../src/pages/brok.js" /* webpackChunkName: "component---src-pages-brok-js" */),
-  "component---src-pages-c-1-js": () => import("./../../../src/pages/c1.js" /* webpackChunkName: "component---src-pages-c-1-js" */),
-  "component---src-pages-c-2-js": () => import("./../../../src/pages/c2.js" /* webpackChunkName: "component---src-pages-c-2-js" */),
   "component---src-pages-index-js": () => import("./../../../src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */),
   "component---src-pages-pdf-gen-js": () => import("./../../../src/pages/pdf_gen.js" /* webpackChunkName: "component---src-pages-pdf-gen-js" */),
   "component---src-pages-pdf-out-js": () => import("./../../../src/pages/pdf_out.js" /* webpackChunkName: "component---src-pages-pdf-out-js" */),
   "component---src-pages-rekning-js": () => import("./../../../src/pages/rekning.js" /* webpackChunkName: "component---src-pages-rekning-js" */),
-  "component---src-pages-t-2-js": () => import("./../../../src/pages/t2.js" /* webpackChunkName: "component---src-pages-t-2-js" */),
   "component---src-pages-tital-js": () => import("./../../../src/pages/tital.js" /* webpackChunkName: "component---src-pages-tital-js" */),
   "component---src-pages-using-typescript-tsx": () => import("./../../../src/pages/using-typescript.tsx" /* webpackChunkName: "component---src-pages-using-typescript-tsx" */)
 }
