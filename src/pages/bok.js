f29569f544b62ef50853573f73a1c3567a1933b4
sindre <sindre.heggen@gmail.com>
2021-08-16 06:58:30 +0200

    added AM to bok

--- src/pages/bok.js
+++ src/pages/bok.js
@@ -4,8 +4,13 @@ import Layout from "../components/layout"
 
 const IndexPage = () => (
   <Layout>
-	<a href="https://drive.google.com/file/d/1YqmxRJgP2lblnrMPUT8qfH2nhcNPWJIv/view?usp=sharing"> Matematikken sine byggesteinar</a> 
+	<a href="https://github.com/sindrsh/FirstPrinciplesOfMath/blob/master/MB.pdf"> Matematikken sine byggesteinar</a> 
+	<p> Tek for seg teorigrunnlaget for matematikken </p>
+	<p></p>
+	<a href="https://github.com/sindrsh/FirstPrinciplesOfMath/blob/master/MB.pdf"> Anvendt matematikk for grunnskole og VGS</a> 
+	<p> Tek for seg anvendingar. Dekker tema f.o.m. 5. klasse til 2P på VGS. (Nynorskversjon kjem innan 22. august.) </p>
   </Layout>
 )
+
 
 export default IndexPage
